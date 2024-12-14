0acdd7bcd820f732845b070dc5800e7e67dc468c
pkino <pkino@pkino.net>
2024-12-14 13:34:23 +0900

    use flakes

---
+++ flake.nix
@@ -0,0 +1,16 @@
+{
+  description = "pkino's NixOS flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+  };
+
+  outputs = { self, nixpkgs, ... }@inputs: {
+    nixosConfigurations.pkino-desktop = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        ./configuration.nix
+      ];
+    };
+  };
+}
