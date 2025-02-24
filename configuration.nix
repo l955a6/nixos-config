205d52f114f0e3dbdc8e580e376c1513b6c037c0
pkino <pkino@pkino.net>
2025-02-25 04:09:20 +0900

    enable X11 instead of Wayland

--- configuration.nix
+++ configuration.nix
@@ -56,6 +56,7 @@
 
   # Enable the GNOME Desktop Environment.
   services.xserver.displayManager.gdm.enable = true;
+  services.xserver.displayManager.gdm.wayland = false;
   services.xserver.desktopManager.gnome.enable = true;
 
   # Configure keymap in X11
