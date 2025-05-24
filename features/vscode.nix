c4508a0a5dd47d2bf53d953d77703813c934428b
pkino <pkino@pkino.net>
2025-05-24 14:40:02 +0900

    Nix言語のLSPやformatterをVSCodeで使えるようにした

--- features/vscode.nix
+++ features/vscode.nix
@@ -1,4 +1,4 @@
-{ nix-vscode-extensions, ... }: {
+{ pkgs, nix-vscode-extensions, ... }: {
   programs.vscode = {
     enable = true;
 
@@ -12,16 +12,30 @@
       "editor.rulers" = [ 100 ];
       "editor.fontFamily" = "Cica";
       "editor.fontSize" = 22;
+      "editor.renderWhitespace" = "boundary";
       "terminal.integrated.fontFamily" = "Cica";
+      "files.eol" = "\n";
+
+      # Nix IDE config
+      "nix.enableLanguageServer" = true;
+      "nix.serverPath" = "${pkgs.nixd}/bin/nixd";
+      "nix.serverSettings" = {
+        "formatting" = {
+	  "command" = ["nixfmt"];
+	};
+      };
     };
 
     extensions = let
       vscode-marketplace = nix-vscode-extensions.extensions."x86_64-linux".vscode-marketplace;
     in with vscode-marketplace; [
+      # theme
       catppuccin.catppuccin-vsc
       catppuccin.catppuccin-vsc-icons
+      # lsp clients
+      jnoortheen.nix-ide
+      # misc
       asvetliakov.vscode-neovim
-
       marp-team.marp-vscode
       gera2ld.markmap-vscode
     ];
